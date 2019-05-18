b9c1eaa4ddbc5378e516dace0c5a0eb4327bede2
Robin Khiv <robinkhiv@gmail.com>
2019-05-17 18:08:55 -0700

    fix cors issue with server

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 'use strict';
-
-    
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/petful',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://petful-duo-app.robinkhiv.now.sh'
 };
