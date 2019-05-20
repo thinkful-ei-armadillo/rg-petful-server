0eea12f9bb37048e5bb52159458e44e2d9b98c4f
Robin Khiv <robinkhiv@gmail.com>
2019-05-20 12:47:11 -0700

    added client address for cors

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/petful',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://petful-duo-app.robinkhiv.now.sh'
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://petful-app.robinkhiv.now.sh'
 };
